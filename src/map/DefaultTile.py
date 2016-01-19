1f21cc4faabff1924198bb59b29dd4936baba1d7
Tibenede <tibenede@gmail.com>
2016-01-19 23:00:43 +0100

    gitignore + defaultTile

--- src/map/DefaultTile.py
+++ src/map/DefaultTile.py
@@ -1,9 +1,10 @@
 # -*-coding:UTF-8 -*
 """Default Tile"""
 
-class DefaultTile:
+class DefaultTile(pygame.sprite.Sprite):
 
-    def __init__(self, number=0, name="", path=""):
+    def __init__(self, number=0, name="", path="", *groups):
+        pygame.sprite.Sprite.__init__(self, *groups)
         self._number = number
         self._name = name
         self._path = path
